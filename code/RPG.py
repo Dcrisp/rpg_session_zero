70569bae6bbb5dc8905ff58ba2727511ff7f6fd7
Dcrisp <daniel.crisp1994@gmail.com>
2023-06-18 21:18:22 +0100

    integrated buy function into larger decision tree and experimented with a sell function

--- code/RPG.py
+++ code/RPG.py
@@ -1,6 +1,14 @@
 
 #Shop conversation
-player_purse = 7
+
+village_choice_list = ['enter the shop', 'go to the town square', 'Duck!', 'go to the castle', 'go the well' ]
+player_inventory_values = {
+    "rusty sword": 2,
+    "ragged tunic": 1,
+    "rope": 3,
+    "old boots": 0.5,
+    "purse": 7,
+}
 shop_inventory_values = {
     "hat": 5,     #hat <4 ch, will not allign with other items
     "gloves": 3,
@@ -8,9 +16,9 @@ shop_inventory_values = {
     "cape": 11,
     "boots": 10,
 }  # dictionary to recall values
-
-
 # use_snake_case
+
+
 def purse_check(shop_item, item_values, purse_value):
     """check player can afford item
     a function to check whether the player has enough money in their purse to afford the selected item
@@ -36,6 +44,14 @@ def show_shop_inventory(item_values):
     for item in item_values.keys():
         print(f'\t{item} = {item_values[item]} gold')
 
+#def sell(sale_item_values, sale_purse_value):
+    #show_shop_inventory(player_inventory_values)
+    #sale = input('What would you like sell? ')
+    #if sale in sale_item_values.keys():
+        #print(f"a fine piece, I'll give you {sale_item_values[item]} gold for it")
+        #player_inventory_values['purse'] += {sale_item_values[item]}
+        #del sale_item_values.keys[item]
+
 def buy(item_values, purse_value):
     """initial interaction for item selection
 
@@ -45,17 +61,32 @@ def buy(item_values, purse_value):
     shop = input('Would you like to see my wares? ')
     if shop == 'yes':
         show_shop_inventory(item_values)
+        next = input('What would you like to buy? ')
+        if next in item_values.keys():
+            purse_check(shop_item=next, item_values=item_values, purse_value=purse_value)
+        else:
+            print(f"I don't have any {next}s here sunshine!")
+            buy(item_values=item_values,purse_value=purse_value)   #recursion
     if shop == 'no':
-        print('Piss off then, I have other customers waiting!')
+        qu_sell = input('Do you have anything to sell? ')
+        if qu_sell == 'yes':
+            print('cool')
+            #sell(sale_item_values=player_inventory_values, sale_purse_value= player_inventory_values['purse'])
+        elif qu_sell == 'no':
+            print('Piss off then, I have other customers waiting!')
 
-    next = input('What would you like to buy? ')
-    if next in item_values.keys():
-        purse_check(shop_item=next, item_values=item_values, purse_value=purse_value)
-    else:
-        print(f"I don't have any {next}s here sunshine!")
-        buy(item_values=item_values,purse_value=purse_value)   #recursion
 
-buy(item_values=shop_inventory_values, purse_value=player_purse)
+print('Greetings traveller!')
+buy(item_values=shop_inventory_values, purse_value=player_inventory_values['purse'])
+shop_qu1 =input('Anything else? ')
+if shop_qu1 == 'yes':
+    buy(item_values=shop_inventory_values, purse_value=player_inventory_values['purse'])
+if shop_qu1 =='no':
+    print("On your way then, I've got work to do")
+
+print('//////////////////////////////////////////////////////////////////////////////////////////////////')
+
+
 
 import random
 c1ra = 'Craven!'
